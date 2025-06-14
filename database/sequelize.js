dc71f2ddf2eb96a083b11adb28229984ea88aff7
PhamThanhTuan0215 <thanhtuancr1234@gmail.com>
2025-06-14 23:55:04 +0700

    add dialectModule pg

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,8 +1,9 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize, pg } = require('sequelize');
 require('dotenv').config()
 
 const sequelize = new Sequelize(process.env.URI_PRODUCT_SERVICE_DATABASE, {
   dialect: 'postgres',
+  dialectModule: pg,
   logging: false,
 });
 
